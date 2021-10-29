8b02e8eddc9ac38de4d14d77a8535f4f11e73f34
tanakatakasi <okisilolk@gmail.com>
2021-10-29 16:47:52 +0900

    pythonファイルの追加

---
+++ statement_3.py
@@ -0,0 +1,15 @@
+a = 10
+b = 20
+"""
+この範囲はコメントとして
+実行時には無視されます
+"""
+ans = a + b
+print(ans)
+c =30
+print(a)
+print(b)
+print(c)
+abc = a + b + c
+print(a, b, c, sep = "、", end = "\以上")
+print(abc)
